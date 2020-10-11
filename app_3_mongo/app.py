4fe14c39667e4862c5c627f574b6d971a4910dca
Dzmitry Babak <alexgott1990@gmail.com>
2020-10-11 20:17:39 +0300

    add: mongo app

--- app_3_mongo/app.py
+++ app_3_mongo/app.py
@@ -3,12 +3,18 @@ from uuid import uuid4
 
 storage = MongodbService.get_instance()
 
-for _ in range(5):
-    dto = {
-        "_id": str(uuid4()),
-        "payload": str(uuid4()),
-    }
-    storage.save_data(dto)
 
-for data in storage.get_data():
-    print(data)
+def main():
+    for _ in range(5):
+        dto = {
+            "_id": str(uuid4()),
+            "payload": str(uuid4()),
+        }
+        storage.save_data(dto)
+
+    for data in storage.get_data():
+        print(data)
+
+
+if __name__ == '__main__':
+    main()

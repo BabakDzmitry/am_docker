8256b88ed80348a162d68d7444310128e85a1e2d
Dzmitry Babak <alexgott1990@gmail.com>
2020-10-11 20:34:17 +0300

    add: mongo app

--- app_3_mongo/app.py
+++ app_3_mongo/app.py
@@ -1,18 +1,22 @@
-from .storage import MongodbService
 from uuid import uuid4
+from pathlib import Path
+import sys
 
-storage = MongodbService.get_instance()
+main_path = Path(__file__).resolve().parent
+sys.path.insert(0,main_path)
+from storage import MongodbService
 
 
 def main():
+    storage_ = MongodbService.get_instance()
     for _ in range(5):
         dto = {
             "_id": str(uuid4()),
             "payload": str(uuid4()),
         }
-        storage.save_data(dto)
+        storage_.save_data(dto)
 
-    for data in storage.get_data():
+    for data in storage_.get_data():
         print(data)
 
 
